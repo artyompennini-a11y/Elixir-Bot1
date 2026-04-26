f1d1608f97c8404842c411567d9b9a4fb6b78226
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-26 18:56:28 +0200

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -4,19 +4,20 @@ let handler = async (m, { conn, participants }) => {
   let a = member[Math.floor(Math.random() * member.length)]
   let b = member[Math.floor(Math.random() * member.length)]
   
-  // Evitiamo che scelga la stessa persona
   while (b === a) b = member[Math.floor(Math.random() * member.length)]
   
   let love = Math.floor(Math.random() * 100)
-  let nameA = `@${a.split('@')[0]}`
-  let nameB = `@${b.split('@')[0]}`
   
   let caption = `✨ *ELIXIR MATCHMAKER* ✨\n\n`
-  caption += `💞 ${nameA} + ${nameB}\n`
+  caption += `💞 @${a.split('@')[0]} + @${b.split('@')[0]}\n`
   caption += `💓 *Compatibilità:* ${love}%\n\n`
   caption += love > 70 ? "🔥 Una coppia esplosiva!" : "🧊 Forse è meglio restare amici..."
 
-  await conn.sendMessage(m.chat, { text: caption, mentions: [a, b] }, { quoted: m })
+  // Il segreto è passare gli ID nell'array mentions
+  await conn.sendMessage(m.chat, { 
+    text: caption, 
+    mentions: [a, b] 
+  }, { quoted: m })
 }
 
 handler.help = ['ship']
