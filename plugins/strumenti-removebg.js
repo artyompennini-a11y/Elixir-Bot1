316872dcb457464ee47690d0550ff37c11042f88
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-28 00:48:24 +0200

    Update strumenti-removebg.js

--- plugins/strumenti-removebg.js
+++ plugins/strumenti-removebg.js
@@ -74,7 +74,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
             m.chat, 
             processedImg, 
             'removebg.png', 
-            '『 ✨ 』- \`Sfondo rimosso con successo.\`\n\n> `vare ✧ bot`', 
+            '『 ✨ 』- \`Sfondo rimosso con successo.\`\n\n> `elixir ✧ bot`', 
             m
         );
         
