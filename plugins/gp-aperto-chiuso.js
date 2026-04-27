3ab0e7c8aed12c0796376101643f76ee6439ee1c
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-27 16:48:09 +0200

    Update gp-aperto-chiuso.js

--- plugins/gp-aperto-chiuso.js
+++ plugins/gp-aperto-chiuso.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command }) => {
     let isOpen = command === 'aperto'
     await conn.groupSettingUpdate(m.chat, isOpen ? 'not_announcement' : 'announcement')
     await conn.sendMessage(m.chat, {
-        text: isOpen ? '𝐩𝐚𝐫𝐥𝐚𝐭𝐞 𝐩𝐥𝐞𝐛𝐞𝐢' : '𝐨𝐫𝐚 𝐩𝐚𝐫𝐥𝐚𝐧𝐨 𝐠𝐥𝐢 𝐢𝐝𝐞𝐢',
+        text: isOpen ? '𝐩𝐚𝐫𝐥𝐚𝐭𝐞 𝐩𝐥𝐞𝐛𝐞𝐢' : '𝐨𝐫𝐚 𝐩𝐚𝐫𝐥𝐚𝐧𝐨 𝐠𝐥𝐢 𝐝𝐞𝐢',
         contextInfo: {
             forwardingScore: 99,
             isForwarded: true,
