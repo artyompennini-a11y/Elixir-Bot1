000fd3fbfdb5f7ba98c7b6ca9bbadf73e76dc5c3
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-05 14:28:35 +0200

    Update elixir.js

--- plugins/elixir.js
+++ plugins/elixir.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn }) => {
-  const message = `ᴇʟɪxɪʀ ᴇ ɪʟ ꜱɪᴄɪʟɪᴀɴᴏ ᴘɪᴜ ꜱɪᴍᴘᴀᴛɪᴄᴏ ᴅᴇʟʟᴇ ᴄᴏᴍᴍ, ɪʟ ᴍɪɢʟɪᴏʀᴇ.
-ɴᴏɴ ʟᴏ ꜰᴀᴛᴇ ɪɴᴄᴀᴢᴢᴀʀᴇ ᴏ ᴠɪ ꜱᴀʟᴛᴀɴᴏ ɪ ɴᴜᴍᴇʀɪ ᴇ ᴘᴀʀᴛᴏɴᴏ ɪ ᴅᴏxx ᴅᴏᴠᴇ ᴠɪ ᴘʀᴇɴᴅᴇ ᴘᴜʀᴇ ɪ ᴘᴇʟɪ ᴅᴇʟ ᴄᴜʟᴏ.`;
+  const message = `The punisher è il re di zozzap e se hai qualcosa in contrario ritorna a succhiare le tette di mamma fallito.`;
 
   await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 };
 
-handler.help = ['elixir'];
+handler.help = ['punisher2'];
 handler.tags = ['giochi'];
 handler.command = /^(elixir)$/i; // Corretto il regex per attivarsi con "elixir"
 
