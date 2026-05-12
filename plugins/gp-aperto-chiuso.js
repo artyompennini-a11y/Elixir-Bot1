6e72d3f6609f8c521810b99997c251a823b88174
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-12 17:11:20 +0200

    Update gp-aperto-chiuso.js

--- plugins/gp-aperto-chiuso.js
+++ plugins/gp-aperto-chiuso.js
@@ -25,7 +25,7 @@ let handler = async (m, { conn, command }) => {
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363426613954732@newsletter',
+                newsletterJid: '120363407063409735@newsletter',
                 newsletterName: `✦ ${global.db.data.nomedelbot || 'THE PUNISHER-BOT'} ✦`,
                 serverMessageId: 143
             }
