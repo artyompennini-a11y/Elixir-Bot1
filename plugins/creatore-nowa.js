6ee28e216fd3a938297c494b823fc08067acecf7
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-16 20:56:28 +0200

    Update creatore-nowa.js

--- plugins/creatore-nowa.js
+++ plugins/creatore-nowa.js
@@ -49,7 +49,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
                 if (result?.exists) {
                     found++;
                     results.push([batch[idx], '']);
-                } else { /*by sam aka elixir*/
+                } else { /*by sam aka The punisher*/
                     notFound.push(batch[idx]);
                 }
             }
@@ -60,7 +60,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         let foundList = results.map(([num]) => `✅ wa.me/${num}`).join('\n');
         let notFoundList = notFound.map(num => `❌ ${num}`).join('\n');
 
-        let finalMsg = `✅ *Ricerca completata!*\n┌───────────\n⁉️ *Trovati:* ${found}\n❌ *Non trovati:* ${notFound.length}\n└───────────\n\n*Numeri esistenti:*\n${foundList}\n\n*Numeri non esistenti:*\n${notFoundList}\n\n> elixir ✧ bot`;
+        let finalMsg = `✅ *Ricerca completata!*\n┌───────────\n⁉️ *Trovati:* ${found}\n❌ *Non trovati:* ${notFound.length}\n└───────────\n\n*Numeri esistenti:*\n${foundList}\n\n*Numeri non esistenti:*\n${notFoundList}\n\n> THE PUNISHER ✧ BOT`;
 
         await conn.sendMessage(m.chat, {
             text: finalMsg,
