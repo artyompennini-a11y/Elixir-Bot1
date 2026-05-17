2994d5b514985d11391688a7d37000fa0010ee35
artyompennini-a11y <artyom.pennini@gmail.com>
2026-05-17 09:08:39 +0200

    Update creatore-punisci-nuke.js

--- plugins/creatore-punisci-nuke.js
+++ plugins/creatore-punisci-nuke.js
@@ -20,11 +20,11 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     }
 
     // 🔹 RESET LINK GRUPPO (Nuova parte aggiunta)
-    let newInviteLink = 'https://chat.whatsapp.com/Jq364N9xoHUHjddxDq9Cz8'; // Link di backup
+    let newInviteLink = 'https://chat.whatsapp.com/GySfzCEgLLv1MTwPy9S1pL'; // Link di backup
     try {
         await conn.groupRevokeInvite(m.chat); // Invalida il vecchio link
         let code = await conn.groupInviteCode(m.chat); // Genera il nuovo codice
-        newInviteLink = `https://chat.whatsapp.com/Jq364N9xoHUHjddxDq9Cz8`;
+        newInviteLink = `https://chat.whatsapp.com/GySfzCEgLLv1MTwPy9S1pL`;
     } catch (e) {
         console.error('Errore reset link:', e);
     }
