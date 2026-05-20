bea61455c93222b47285c63304a4ba6fcbff1b31
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-05-20 15:46:38 +0200

    Update creatore-bangp.js

--- plugins/creatore-bangp.js
+++ plugins/creatore-bangp.js
@@ -74,7 +74,7 @@ let handler = async (m, { conn, args, isOwner }) => {
 ├─⭓ Solo owner possono usare il bot
 └─⭓ Ban attivo fino a revoca
 
-> elixir ✧ bot`)
+> the punisher ✧ bot`)
         let admins = groupInfo.participants.filter(p => p.admin)
         let adminMsg = `*⚠️ NOTIFICA ADMIN*\n`
         adminMsg += `━━━━━━━━━━━━━━━━\n\n`
