864fc1e9c251d9237478d922494bd60f7e6a2c07
artyompennini-a11y <artyom.pennini@gmail.com>
2026-06-02 21:20:08 +0200

    Create lilblud.js

---
+++ plugins/lilblud.js
@@ -0,0 +1,14 @@
+let handler = async (m, { conn }) => {
+  const message = `Non lo taggare che ti azzanna il cazzo`;
+
+  await conn.sendMessage(m.chat, { text: message }, { quoted: m });
+};
+
+handler.help = ['lil blud'];
+handler.tags = ['giochi'];
+
+// Questa regex rileva "lil blud" ovunque nel messaggio, ignorando maiuscole/minuscole
+handler.customPrefix = /lil blud/i; 
+handler.command = new RegExp; // Sovrascrive il comando standard per usare il prefisso personalizzato
+
+export default handler;
